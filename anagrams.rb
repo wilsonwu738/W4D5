03c431682ee077247e97ecdd2a684f005fa90b04
Joey Tsui <joeytsui131@gmail.com>
2022-12-02 15:30:28 -0500

    anagrams done

--- anagrams.rb
+++ anagrams.rb
@@ -1,5 +1,17 @@
+# Phase 1
 
+def first_anagram?(str_1, str_2)
+  arr = str_1.chars.permutation.to_a
+  arr2 = str_2.chars
 
+  return false if !arr.include?(arr2)
+  true
+end
+
+a = "abcdefg"
+b = "gfedabcy"
+
+p first_anagram?(a, b)
 
 #phase 2
 def second_anagram?(str_1, str_2)
@@ -32,4 +44,21 @@ end
 a = "abcdefa"
 b = "gfedabc"
 
-p third_anagram?(a, b)
+# p third_anagram?(a, b)
+
+def fourth_anagram?(str_1, str_2)
+  return false if str_1.length != str_2.length
+  hash = Hash.new(0)
+
+  str_1.each_char.with_index do |char, i|
+    hash[str_1[i]] += 1
+    hash[str_2[i]] -= 1
+  end
+
+  hash.values.all?(0)
+end
+
+a = "abcdefgy"
+b = "gfedabcy"
+
+# p fourth_anagram?(a, b)
